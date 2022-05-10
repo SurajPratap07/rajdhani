435c2f2150de71964ce8750d0fd358acea982e82
SurajPratap07 <105055042+SurajPratap07@users.noreply.github.com>
2022-05-10 00:23:33 -0700

    Initial commit for Rajdhani

---
+++ src/DemoForRajdhani.java
@@ -0,0 +1,12 @@
+public class DemoForRajdhani {
+    public static void main(String[] args) {
+        System.out.println("hello mummy");
+        System.out.println("welcome sir");
+
+        int age = 28;
+                System.out.println("printing age"+ age);
+
+
+
+    }
+}
